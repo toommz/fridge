8f431c6cb94e65b09ee3cb2dd9aa8acc4bd8ca93
Thomas Battiston <thomas.battiston@wttj.co>
2022-05-19 14:42:29 +0200

    fix: adjust layout

--- app/javascript/entrypoint.js
+++ app/javascript/entrypoint.js
@@ -30,7 +30,7 @@ const App = () => {
 
   return (
     <>
-      <form className="m-4 flex" onSubmit={handleSubmit}>
+      <form className="max-w-sm m-4 flex content-center justify-center mx-auto" onSubmit={handleSubmit}>
         <input className="rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white" placeholder="Ingredients: peanut butter,flour,sugar" type="text" value={formData.ingredients} onChange={(event) => setFormData({...formData, ingredients: event.target.value })} />
         <input className="px-8 rounded-r-lg bg-yellow-400  text-gray-800 font-bold p-4 uppercase border-yellow-500 border-t border-b border-r" type="submit" value="Gimme dinner ideas" />
       </form>
